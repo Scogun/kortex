961615cade6ef0de7fb9598cdd87ef9eb9bb1cfb
Sergey <AntonovSergeyS@gmail.com>
2026-03-01 21:33:28 +0100

    Extend Light

--- kortex-core/src/main/kotlin/com/ucasoft/kortex/entities/Light.kt
+++ kortex-core/src/main/kotlin/com/ucasoft/kortex/entities/Light.kt
@@ -2,8 +2,13 @@ package com.ucasoft.kortex.entities
 
 import com.ucasoft.kortex.client.KortexContext
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 class Light(stateFlow: StateFlow<State>, context: KortexContext): ToggleableEntity<LightAttributes>(stateFlow, context) {
 
@@ -30,8 +35,89 @@ class Light(stateFlow: StateFlow<State>, context: KortexContext): ToggleableEnti
 
 @Serializable
 open class LightAttributes(
+    val brightness: Int? = null,
     @SerialName("color_mode")
-    val colorMode: String? = null,
+    val colorMode: ColorMode? = null,
+    @SerialName("color_temp_kelvin")
+    val colorTempKelvin: Int? = null,
+    val effect: String? = null,
+    @SerialName("effect_list")
+    val effectList: List<String>? = null,
+    @SerialName("hs_color")
+    val hsColor: HsColor? = null,
+    @SerialName("max_color_temp_kelvin")
+    val maxColorTempKelvin: Int? = null,
+    @SerialName("min_color_temp_kelvin")
+    val minColorTempKelvin: Int? = null,
+    @SerialName("rgb_color")
+    val rgbColor: RgbColor? = null,
+    @SerialName("rgbw_color")
+    val rgbwColor: RgbwColor? = null,
+    @SerialName("rgbww_color")
+    val rgbwwColor: RgbwwColor? = null,
     @SerialName("supported_color_modes")
     val supportedColorModes: List<String>,
+    @SerialName("xy_color")
+    val xyColor: XyColor? = null
 ): FeaturedAttributes()
+
+enum class ColorMode {
+    UNKNOWN,
+    ONOFF,
+    BRIGHTNESS,
+    COLOR_TEMP,
+    HS,
+    RGB,
+    RGBW,
+    RGBWW,
+    WHITE,
+    XY
+}
+
+@Serializable(with = ColorSerializers.HS::class)
+data class HsColor(val hue: Float, val saturation: Float)
+
+@Serializable(with = ColorSerializers.RGB::class)
+data class RgbColor(val red: Int, val green: Int, val blue: Int)
+
+@Serializable(with = ColorSerializers.RGBW::class)
+data class RgbwColor(val red: Int, val green: Int, val blue: Int, val white: Int)
+
+@Serializable(with = ColorSerializers.RGBWW::class)
+data class RgbwwColor(val red: Int, val green: Int, val blue: Int, val coldWhite: Int, val warmWhite: Int)
+
+@Serializable(with = ColorSerializers.XY::class)
+data class XyColor(val x: Float, val y: Float)
+
+object ColorSerializers {
+    // Шаблон для десериализации массива в объект
+    abstract class ArraySerializer<T, V>(elementSerializer: KSerializer<V>) : KSerializer<T> {
+        protected val delegate = ListSerializer(elementSerializer)
+        override val descriptor = delegate.descriptor
+    }
+
+    object HS : ArraySerializer<HsColor, Float>(Float.serializer()) {
+        override fun serialize(encoder: Encoder, value: HsColor) = encoder.encodeSerializableValue(delegate, listOf(value.hue, value.saturation))
+        override fun deserialize(decoder: Decoder) = delegate.deserialize(decoder).let { HsColor(it[0], it[1]) }
+    }
+
+    object RGB : ArraySerializer<RgbColor, Int>(Int.serializer()) {
+        override fun serialize(encoder: Encoder, value: RgbColor) = encoder.encodeSerializableValue(delegate, listOf(value.red, value.green, value.blue))
+        override fun deserialize(decoder: Decoder) = delegate.deserialize(decoder).let { RgbColor(it[0], it[1], it[2]) }
+    }
+
+    object RGBW : ArraySerializer<RgbwColor, Int>(Int.serializer()) {
+        override fun serialize(encoder: Encoder, value: RgbwColor) = encoder.encodeSerializableValue(delegate, listOf(value.red, value.green, value.blue, value.white))
+        override fun deserialize(decoder: Decoder) = delegate.deserialize(decoder).let { RgbwColor(it[0], it[1], it[2], it[3]) }
+    }
+
+    object RGBWW : ArraySerializer<RgbwwColor, Int>(Int.serializer()) {
+        override fun serialize(encoder: Encoder, value: RgbwwColor) = encoder.encodeSerializableValue(delegate, listOf(value.red, value.green, value.blue, value.coldWhite, value.warmWhite))
+        override fun deserialize(decoder: Decoder) = delegate.deserialize(decoder).let { RgbwwColor(it[0], it[1], it[2], it[3], it[4]) }
+    }
+
+    object XY : ArraySerializer<XyColor, Float>(Float.serializer()) {
+        override fun serialize(encoder: Encoder, value: XyColor) = encoder.encodeSerializableValue(delegate, listOf(value.x, value.y))
+        override fun deserialize(decoder: Decoder) = delegate.deserialize(decoder).let { XyColor(it[0], it[1]) }
+    }
+}
