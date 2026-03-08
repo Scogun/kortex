767f30319aab96fc0c0cac8974c0629849751352
Sergey <AntonovSergeyS@gmail.com>
2026-03-08 16:52:23 +0100

    Add press/onPressed method

--- kortex-core/src/main/kotlin/com/ucasoft/kortex/entities/Button.kt
+++ kortex-core/src/main/kotlin/com/ucasoft/kortex/entities/Button.kt
@@ -1,13 +1,29 @@
 package com.ucasoft.kortex.entities
 
 import com.ucasoft.kortex.client.KortexContext
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.serialization.SerialName
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.Serializable
 
 class Button(stateFlow: StateFlow<State>, context: KortexContext): Entity<ButtonAttributes>(stateFlow, context) {
 
     override val attributesFlow = mapAttributes<ButtonAttributes>()
+
+    suspend fun press() = callService("press")
+
+    fun onPressed(
+        block: suspend Button.() -> Unit
+    ) = onPressed(context.scope, block)
+
+    fun onPressed(
+        scope: CoroutineScope,
+        block: suspend Button.() -> Unit
+    ) = observe {
+        it.state
+    }.drop(1).onEach { block() }.launchIn(scope)
 }
 
 @Serializable
