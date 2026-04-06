9b737a6bf0830745a2e9b7967cd6f0ec2b139bd2
Sergey <AntonovSergeyS@gmail.com>
2026-04-06 17:00:33 +0200

    Fix Sensor

--- kortex-sample/src/commonMain/kotlin/com/ucasoft/kortex/sample/compose/Sensor.kt
+++ kortex-sample/src/commonMain/kotlin/com/ucasoft/kortex/sample/compose/Sensor.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import com.ucasoft.kortex.compose.extensions.collectAsState
 import com.ucasoft.kortex.entities.Sensor
 import com.ucasoft.kortex.entities.SensorDeviceClass
+import com.ucasoft.kortex.entities.SensorStateClass
 import kotlin.time.Instant
 
 @Composable
@@ -25,9 +26,11 @@ private fun RowScope.Sensor(sensor: Sensor) {
     ) {
         when (state.attributes.deviceClass) {
             SensorDeviceClass.BATTERY -> "${state.getStateAs<Int>()} ${state.attributes.unit}"
-            SensorDeviceClass.HUMIDITY, SensorDeviceClass.TEMPERATURE, SensorDeviceClass.VOLTAGE  -> "${state.getStateAs<Double>()} ${state.attributes.unit}"
             SensorDeviceClass.TIMESTAMP -> state.getStateAs<Instant>().toString()
-            else -> state.getStateAs<String>()
+            else -> when (state.attributes.stateClass) {
+                SensorStateClass.MEASUREMENT -> "${state.getStateAs<Double>()} ${state.attributes.unit}"
+                else -> state.getStateAs<String>()
+            }
         }?.let {
             EntityState(it)
         }
